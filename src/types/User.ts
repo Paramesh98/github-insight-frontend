441c5679678eccc2760796b1db0929055ce50ff5
Paramesh Krishna <parameshkrishna98@gmail.com>
2024-12-27 12:00:36 +0100

    feat: move pagination logic to backend

--- src/types/User.ts
+++ src/types/User.ts
@@ -16,7 +16,7 @@ export type User = {
   html_url?: string;
   id?: number;
   location?: string;
-  login?: string;
+  login: string;
   name?: string;
   node_id?: string;
   organizations_url?: string;
